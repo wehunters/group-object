5fd9f2322fa64826ff79a7a5e00ec450e184efc0
Anjali <anjali@ghcbp.com>
2020-04-01 17:07:02 +0530

    files updated

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-exports.printMsg = function(text) {
-    console.log("This is a message from the demo package",text);
-  }
+exports.group = function(Arr,key) {
+    let output ={};
+    for (let value of Arr) {
+        if(!output[value[key]]){
+            output[value[key]]=[];
+        }
+        if(value[key]){
+            output[value[key]].push(value);
+        }            
+    }
+    return output;    
+}
